3fdf9ea3f80f17cf2616714fb463c72eb6943acd
Ayush8923 <ayush.uniyal@coloredcow.in>
2023-06-30 20:12:34 +0530

    :zap: fix the ESLine CI checks

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,9 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
-    'X-Requested-With': "XMLHttpRequest",
+    'X-Requested-With': 'XMLHttpRequest',
   },
   withCredentials: true,
 });

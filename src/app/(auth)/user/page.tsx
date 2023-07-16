9e6612a15776d595831cd878de61fb0d8a12085a
Ayush8923 <ayush.uniyal@coloredcow.in>
2023-07-16 21:07:24 +0530

    :art: add the delete functionality and also remove the some duplicate code

--- src/app/(auth)/user/page.tsx
+++ src/app/(auth)/user/page.tsx
@@ -1,31 +1,43 @@
 'use client';
 
-import { getUsers } from '@/apis/user';
+import { getUsers, deleteUser } from '@/apis/user';
 import Table from '@/components/Table';
-import { User, TableData } from '@/utils/types';
+import { UserResource, TableData } from '@/utils/types';
 import { useEffect, useState } from 'react';
 import { PencilSquareIcon, TrashIcon } from '@heroicons/react/24/solid';
 import { UsersIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { toast } from 'react-toastify';
 
 const Page = () => {
-  const [users, setUsers] = useState<User | undefined>(undefined);
+  const [users, setUsers] = useState<UserResource | undefined>(undefined);
 
   const [tableData, setTableData] = useState<TableData>({
     headers: ['Name', 'Email'],
     rows: [],
   });
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      setUsers(await getUsers());
-    };
-    fetchUsers();
-  }, []);
+  const fetchUsers = async () => {
+    setUsers(await getUsers());
+  };
 
   useEffect(() => {
+    fetchUsers();
+    const showDeleteConfirmationModal = async (id: number) => {
+      const result = confirm('Are you sure you want to delete this user?');
+      if (result) {
+        try {
+          await deleteUser(id);
+          toast.success('User deleted successfully');
+          fetchUsers();
+        } catch (error: any) {
+          toast.error(error.response.data.message);
+        }
+      }
+    };
+
     if (users) {
-      const rows = users.data.map((user: User) => {
+      const rows = users.data.map((user: any) => {
         return [
           <>
             <div>{user.name}</div>
@@ -44,7 +56,11 @@ const Page = () => {
               >
                 <PencilSquareIcon className="h-5 w-5" />
               </Link>
-              <a href="#" className="ml-2 text-gray-400 hover:text-red-600">
+              <a
+                type="button"
+                onClick={() => showDeleteConfirmationModal(user.id)}
+                className="ml-2 text-gray-400 hover:text-red-600"
+              >
                 <TrashIcon className="h-5 w-5" />
               </a>
             </div>
